ea566f3abffb18f0e79aa57c9400e32bf105918b
Fiete Börner <fieteboerner@googlemail.com>
2017-03-19 14:20:13 +0100

    add specific border to classic theme

--- themes/classic/classic_theme.py
+++ themes/classic/classic_theme.py
@@ -1,5 +1,14 @@
 import neitris_theme_base
+import neitris_data
+import os
+
+SPECIAL_BORDER = neitris_data.POWERUP_MIN - 1
 
 
 class Theme(neitris_theme_base.BaseTheme):
-    pass
+
+    borderBrick = SPECIAL_BORDER
+
+    def load_special_bricks(self):
+        path = os.path.join(".", "themes", self.name, "border.png")
+        self.bricks[SPECIAL_BORDER-1] = self._get_image_by_path(path)
