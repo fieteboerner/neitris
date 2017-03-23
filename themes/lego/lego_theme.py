53d3d20871e980f0b0eed2be5cd6045bafaa6502
Arne Uplegger <arne@uplegger.eu>
2017-03-23 15:08:36 +0100

    Adjust Lego Theme to new theming parameters

--- themes/lego/lego_theme.py
+++ themes/lego/lego_theme.py
@@ -1,5 +1,18 @@
 import neitris_theme_base
+import neitris_data
+import os
 
+SPECIAL_BORDER = neitris_data.POWERUP_MIN - 1
 
 class Theme(neitris_theme_base.BaseTheme):
-    pass
+
+    bgColor = (229, 229, 229)
+    matrixBgColor = (16, 155, 9)
+    textColor = (0, 0, 0)
+    gridColor = (30, 165, 10)
+
+    borderBrick = SPECIAL_BORDER
+
+    def load_special_bricks(self):
+        path = os.path.join(".", "themes", self.name, "border.png")
+        self.bricks[SPECIAL_BORDER - 1] = self._get_image_by_path(path)
